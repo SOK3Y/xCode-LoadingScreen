1f5e80a41fe74e8694861f9f5081acdf11e8ca0d
sokey <126024643+SOK3Y@users.noreply.github.com>
2024-07-30 19:09:04 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'sokey'
+description 'xc-loadingscreen'
+
+loadscreen 'html/index.html'
+loadscreen_manual_shutdown "yes"
+
+client_scripts {
+    'client/*.lua'
+}
+
+files {
+    'html/logo.png',
+    'html/background.png',
+    'html/index.html',
+    'html/style.css',
+    'html/script.js',
+    'html/loading.mp3'
+}
